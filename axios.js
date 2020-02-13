cd89c7ae43e17da6b93fd464f521f57df78a439e
klhelker <57188023+klhelker@users.noreply.github.com>
2020-02-13 13:44:34 -0600

    adding hidden information

--- axios.js
+++ axios.js
@@ -1,60 +1,63 @@
+require("dotenv").config()
 
-var axios = require("axios");
+console.log(process.env.USER_NAME)
+console.log(process.env.USER_EMAIL)
+// var axios = require("axios");
 
-// Store all of the arguments in an array
-var nodeArgs = process.argv;
+// // Store all of the arguments in an array
+// var nodeArgs = process.argv;
 
-// Create an empty variable for holding the movie name
-var movieName = "";
+// // Create an empty variable for holding the movie name
+// var movieName = "";
 
-// Loop through all the words in the node argument
-// And do a little for-loop magic to handle the inclusion of "+"s
-for (var i = 2; i < nodeArgs.length; i++) {
+// // Loop through all the words in the node argument
+// // And do a little for-loop magic to handle the inclusion of "+"s
+// for (var i = 2; i < nodeArgs.length; i++) {
 
-  if (i > 2 && i < nodeArgs.length) {
-    movieName = movieName + "+" + nodeArgs[i];
-  } else {
-    movieName += nodeArgs[i];
+//   if (i > 2 && i < nodeArgs.length) {
+//     movieName = movieName + "+" + nodeArgs[i];
+//   } else {
+//     movieName += nodeArgs[i];
 
-  }
-}
+//   }
+// }
 
-// Then run a request with axios to the OMDB API with the movie specified
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+// // Then run a request with axios to the OMDB API with the movie specified
+// var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
 
-// This line is just to help us debug against the actual URL.
-console.log(queryUrl);
+// // This line is just to help us debug against the actual URL.
+// console.log(queryUrl);
 
-axios.get("http://www.artists.bandsintown.com/bandsintown-ap").then(
-  function(response) {
-    console.log(response);
-  })
-axios.get("http://www.omdbapi.com").then(function(response) {
-    console.log(response);
-})
-axios.get("https://www.npmjs.com/package/node-spotify-api").then(
-    function(response) {
-        console.log(response);
-    }
-)
-axios.get()
-  .catch(function(error) {
-    if (error.response) {
-      // The request was made and the server responded with a status code
-      // that falls out of the range of 2xx
-      console.log("---------------Data---------------");
-      console.log(error.response.data);
-      console.log("---------------Status---------------");
-      console.log(error.response.status);
-      console.log("---------------Status---------------");
-      console.log(error.response.headers);
-    } else if (error.request) {
-      // The request was made but no response was received
-      // `error.request` is an object that comes back with details pertaining to the error that occurred.
-      console.log(error.request);
-    } else {
-      // Something happened in setting up the request that triggered an Error
-      console.log("Error", error.message);
-    }
-    console.log(error.config);
-  });
+// axios.get("http://www.artists.bandsintown.com/bandsintown-ap").then(
+//   function(response) {
+//     console.log(response);
+//   })
+// axios.get("http://www.omdbapi.com").then(function(response) {
+//     console.log(response);
+// })
+// axios.get("https://www.npmjs.com/package/node-spotify-api").then(
+//     function(response) {
+//         console.log(response);
+//     }
+// )
+// axios.get()
+//   .catch(function(error) {
+//     if (error.response) {
+//       // The request was made and the server responded with a status code
+//       // that falls out of the range of 2xx
+//       console.log("---------------Data---------------");
+//       console.log(error.response.data);
+//       console.log("---------------Status---------------");
+//       console.log(error.response.status);
+//       console.log("---------------Status---------------");
+//       console.log(error.response.headers);
+//     } else if (error.request) {
+//       // The request was made but no response was received
+//       // `error.request` is an object that comes back with details pertaining to the error that occurred.
+//       console.log(error.request);
+//     } else {
+//       // Something happened in setting up the request that triggered an Error
+//       console.log("Error", error.message);
+//     }
+//     console.log(error.config);
+//   });
