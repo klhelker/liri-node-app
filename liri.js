fec75afe03f88d8c432dd6b8b9a14d7300bc9950
klhelker <57188023+klhelker@users.noreply.github.com>
2020-02-17 16:30:12 -0600

    adding information

--- liri.js
+++ liri.js
@@ -27,84 +27,95 @@ switch(userCommand){
 
 
 }
-// function concertThis(artist){
-//  axios.get("https://rest.bandsintown.com/artists/" + artist + "events?app_id=codingbootcamp").then(
-//   function(response) {
+function concertThis(artist){
+ axios.get("https://rest.bandsintown.com/artists/" + artist + "events?app_id=codingbootcamp").then(
+  function(response) {
    
-//     console.log(response);
-//   })
+    console.log(response);
+  })
 
-// }
+}
 
 
-// function spotifySong(songName){
-//   spotify.search(
-//     {
-//       type: "track",
-//       query: songName
-//     }, 
-//     function(err,data){
-//       if (err) throw err;
+function spotifySong(songName){
+  spotify.search(
+    {
+      type: "track",
+      query: songName
+    }, 
+    function(err,data){
+      if (err) throw err;
 
-//       for(var i = 0; i < data.tracks.items.length; i++)
-//         console.log(data.tracks.items[i].name)
-//       }
-//   )
-// }
+      for(var i = 0; i < data.tracks.items.length; i++)
+        console.log(data.tracks.items[i].name)
+      }
+  )
+}
+
 
 function movieThis(movieName){
-   axios.get("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy").then(function (response) {
+  axios.get("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy").then(function (response) {
 
-      console.log(response);
+
+   
+    console.log("Title: " + response.data.Title);
+    console.log("Release Year: " + response.data.Year);
+    console.log("Country: " + response.data.Country);
+    console.log("Imdb Ratings: " + response.data.imdbRating);
     
-   });
- }
-// * Title of the movie.
-// * Year the movie came out.
-// * IMDB Rating of the movie.
-// * Rotten Tomatoes Rating of the movie.
-// * Country where the movie was produced.
-// * Language of the movie.
-// * Plot of the movie.
-// * Actors in the movie
+    console.log("Rotten Tomatoes: " + response.data.Ratings[1].Value);
+    console.log("Language: " + response.data.Language);
+    console.log("Actors: " + response.data.Actors);
+    console.log("Plot: " + response.data.Plot)
+  })
+  .catch(function(error) {
+    if (error.response) {
+      
+      console.log("---------------Data---------------");
+      console.log(error.response.data);
+      console.log("---------------Status---------------");
+      console.log(error.response.status);
+      console.log("---------------Status---------------");
+      console.log(error.response.headers);
+    } else if (error.request) {
+      console.log(error.request);
+    } else {
+      console.log("Error", error.message);
+    }
+      console.log(error.config);
 
+  });
+}
 
 // Using the `fs` Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
       //  It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
-      //  Edit the text in random.txt to test out the feature for movie-this and concert-this.
+//       //  Edit the text in random.txt to test out the feature for movie-this and concert-this.
 
-// fs.readFile("random.txt", "utf8", function(error, data) {
+fs.readFile("random.txt", "utf8", function(error, data) {
 
-//     // If the code experiences any errors it will log the error to the console.
-//     if (error) {
-//       return console.log(error);
-//     }
+    // If the code experiences any errors it will log the error to the console.
+    if (error) {
+      return console.log(error);
+    }
 
-//     var dataArr = data.split(", ");
+    var dataArr = data.split(", ");
 
-//     // We will then re-display the content as an array for later use.
-//     console.log(dataArr);
+    // We will then re-display the content as an array for later use.
+    console.log(dataArr);
   
-// });
+});
 
 
-// function doWhatItSays() {
+function doWhatItSays() {
   
-//   fs.appendFile("random.txt", ", " , movieName, artist, function(err) {
-//     if (err) {
-//       return console.log(err);
-//     }
+  fs.appendFile("random.txt", ", " , movieName, artist, function(err) {
+    if (err) {
+      return console.log(err);
+    }
   
 
-//     else {
-//     console.log("Content Added!");
-//   }
-//   });
-
-// }
-
- 
-  
-  
-      
-
+    else {
+    console.log("Content Added!");
+  }
+  })
+}
