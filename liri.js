43bda300c6e0da03e2a2d576699db706e14fdf1e
klhelker <57188023+klhelker@users.noreply.github.com>
2020-02-17 11:54:22 -0600

    updates

--- liri.js
+++ liri.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 
+var fs = require("fs");
 var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
 var axios = require("axios");
@@ -26,91 +27,84 @@ switch(userCommand){
 
 
 }
-function concertThis(artist){
- axios.get(`https://rest.bandsintown.com/artists/${artist}events?app_id=codingbootcamp`).then(
-  function(response) {
-    // Then we print out the imdbRating
-    console.log(response);
-  }
-);
-}
-
-
-function spotifySong(songName){
-  spotify.search(
-    {
-      type: "track",
-      query: songName
-    }, 
-    function(err,data){
-      if (err) throw err;
-
-      for(var i = 0; i < data.tracks.items.length; i++)
-        console.log(data.tracks.items[i].name)
-      }
-  )
-}
-
-//spotifySong("Help")
-
-// spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-//   if (err) {
-//     return console.log('Error occurred: ' + err);
-//   }
- 
-// console.log(data); 
-// });
-// spotify
-//   .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
-//   .then(function(data) {
-//     console.log(data); 
-//   })
-//   .catch(function(err) {
-//     console.error('Error occurred: ' + err); 
-//   });
-
-// // Store all of the arguments in an array
-// var nodeArgs = process.argv;
-
-// // Create an empty variable for holding the artist name
-// var artist = "";
-
-// // Loop through all the words in the node argument
-// // And do a little for-loop magic to handle the inclusion of "+"s
-// for (var i = 2; i < nodeArgs.length; i++) {
-
-//   if (i > 2 && i < nodeArgs.length) {
-//     artist = artist + "+" + nodeArgs[i];
-//   } else {
-//     artist += nodeArgs[i];
-//   }};
-
-//   var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
-
-// // This line is just to help us debug against the actual URL.
-// console.log(queryUrl);
-
-// axios.get(queryUrl).then(
+// function concertThis(artist){
+//  axios.get("https://rest.bandsintown.com/artists/" + artist + "events?app_id=codingbootcamp").then(
 //   function(response) {
+   
 //     console.log(response);
 //   })
-//   .catch(function(error) {
-//     if (error.response) {
-//       // The request was made and the server responded with a status code
-//       // that falls out of the range of 2xx
-//       console.log("---------------Data---------------");
-//       console.log(error.response.data);
-//       console.log("---------------Status---------------");
-//       console.log(error.response.status);
-//       console.log("---------------Status---------------");
-//       console.log(error.response.headers);
-//     } else if (error.request) {
-//       // The request was made but no response was received
-//       // `error.request` is an object that comes back with details pertaining to the error that occurred.
-//       console.log(error.request);
-//     } else {
-//       // Something happened in setting up the request that triggered an Error
-//       console.log("Error", error.message);
+
+// }
+
+
+// function spotifySong(songName){
+//   spotify.search(
+//     {
+//       type: "track",
+//       query: songName
+//     }, 
+//     function(err,data){
+//       if (err) throw err;
+
+//       for(var i = 0; i < data.tracks.items.length; i++)
+//         console.log(data.tracks.items[i].name)
+//       }
+//   )
+// }
+
+function movieThis(movieName){
+   axios.get("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy").then(function (response) {
+
+      console.log(response);
+    
+   });
+ }
+// * Title of the movie.
+// * Year the movie came out.
+// * IMDB Rating of the movie.
+// * Rotten Tomatoes Rating of the movie.
+// * Country where the movie was produced.
+// * Language of the movie.
+// * Plot of the movie.
+// * Actors in the movie
+
+
+// Using the `fs` Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
+      //  It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
+      //  Edit the text in random.txt to test out the feature for movie-this and concert-this.
+
+// fs.readFile("random.txt", "utf8", function(error, data) {
+
+//     // If the code experiences any errors it will log the error to the console.
+//     if (error) {
+//       return console.log(error);
 //     }
-//       console.log(error.config);
+
+//     var dataArr = data.split(", ");
+
+//     // We will then re-display the content as an array for later use.
+//     console.log(dataArr);
+  
+// });
+
+
+// function doWhatItSays() {
+  
+//   fs.appendFile("random.txt", ", " , movieName, artist, function(err) {
+//     if (err) {
+//       return console.log(err);
+//     }
+  
+
+//     else {
+//     console.log("Content Added!");
+//   }
 //   });
+
+// }
+
+ 
+  
+  
+      
+
